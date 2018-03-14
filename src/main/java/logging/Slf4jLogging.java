afe3375db924c93f1bf2a5abab3af30f853e29cc
Andrii Piatakha <andrii.piatakha@travisperkins.co.uk>
2018-03-14 14:27:51 +0200

    commit

---
+++ src/main/java/logging/Slf4jLogging.java
@@ -0,0 +1,5 @@
+package logging;
+
+public class Slf4jLogging {
+
+}
---
+++ src/main/java/logging/log4j/Log4jLogging.java
@@ -0,0 +1,103 @@
+package logging.log4j;
+
+public class Log4jLogging {
+	
+//	log4j has three main components:
+//
+//		loggers: Responsible for capturing logging information.
+//
+//		appenders: Responsible for publishing logging information to various preferred destinations.
+//
+//		layouts: Responsible for formatting logging information in different styles.
+//	
+//	It is thread-safe.
+//
+//	It is optimized for speed.
+//
+//	It is based on a named logger hierarchy.
+//
+//	It supports multiple output appenders per logger.
+//
+//	It supports internationalization.
+//
+//	It is not restricted to a predefined set of facilities.
+//
+//	Logging behavior can be set at runtime using a configuration file.
+//
+//	It is designed to handle Java Exceptions from the start.
+//
+//	It uses multiple levels, namely ALL, TRACE, DEBUG, INFO, WARN, ERROR and FATAL.
+//
+//	The format of the log output can be easily changed by extending the Layout class.
+//
+//	The target of the log output as well as the writing strategy can be altered by implementations of the Appender interface.
+//
+//	It is fail-stop. However, although it certainly strives to ensure delivery, log4j does not guarantee that each log statement will be delivered to its destination.
+//
+//	
+//	=========== Support Objects
+//			
+//			Level Object
+//			Filter Object
+//			ObjectRenderer The ObjectRenderer object is specialized in providing a String representation of different objects passed to the logging framework. This object is used by Layout objects to prepare the final logging information.
+//			LogManager The LogManager object manages the logging framework. It is responsible for reading the initial configuration parameters from a system-wide configuration file or a configuration class.
+//	
+//	
+//	
+//	=========== LEVELS
+//			
+//			TRACE
+//			DEBUG
+//			INFO
+//			WARN
+//			ERROR
+//			FATAL
+//			ALL
+//	
+//	
+//	============ APPENDERS properties
+//			
+//			layout	Appender uses the Layout objects and the conversion pattern associated with them to format the logging information.
+//			target	The target may be a console, a file, or another item depending on the appender.
+//			level	The level is required to control the filtration of the log messages.
+//			threshold	Appender can have a threshold level associated with it independent of the logger level. The Appender ignores any logging messages that have a level lower than the threshold level.
+//			filter	The Filter objects can analyze logging information beyond level matching and decide whether logging requests should be handled by a particular Appender or ignored.
+//	
+//	
+//	=========== APPENDERS
+//			
+//			AppenderSkeleton
+//			AsyncAppender
+//			ConsoleAppender
+//			DailyRollingFileAppender
+//			ExternallyRolledFileAppender
+//			FileAppender
+//			JDBCAppender
+//			JMSAppender
+//			LF5Appender
+//			NTEventLogAppender
+//			NullAppender
+//			RollingFileAppender
+//			SMTPAppender
+//			SocketAppender
+//			SocketHubAppender
+//			SyslogAppender
+//			TelnetAppender
+//			WriterAppender
+//	
+//	========== LAYOUTS
+//			
+//			DateLayout
+//			HTMLLayout
+//			PatternLayout
+//			SimpleLayout
+//			XMLLayout
+	
+	
+	
+	
+	
+	
+	
+	
+}

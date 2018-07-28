cbebe034af177bf5c910077634eb4a570a0aa1f1
Andrii Piatakha <andrey.pyatakha@gmail.com>
2018-07-28 22:45:07 +0300

    commit

--- src/main/java/logging/javalogging/examples/LoggerExample.java
+++ src/main/java/logging/javalogging/examples/LoggerExample.java
@@ -1,6 +1,5 @@
 package logging.javalogging.examples;
 
-
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,26 +7,26 @@ import java.util.logging.Logger;
 public class LoggerExample {
 
 	private static final Logger LOGGER = Logger.getLogger(LoggerExample.class.getName());
+
 	public static void main(String[] args) throws SecurityException, IOException {
 
 		LOGGER.setLevel(Level.FINE);
-		LOGGER.info("Logger Name: "+LOGGER.getName());
-		
+		LOGGER.info("Logger Name: " + LOGGER.getName());
+
 		LOGGER.warning("Can cause ArrayIndexOutOfBoundsException");
-		
-		//An array of size 3
-		int []a = {1,2,3};
+
+		// An array of size 3
+		int[] a = { 1, 2, 3 };
 		int index = 4;
-		LOGGER.config("index is set to "+index);
-		
-		try{
+		LOGGER.config("index is set to " + index);
+
+		try {
 			System.out.println(a[index]);
-		}catch(ArrayIndexOutOfBoundsException ex){
+		} catch (ArrayIndexOutOfBoundsException ex) {
+//			LOGGER.severe("Exception occur");
 			LOGGER.log(Level.SEVERE, "Exception occur", ex);
 		}
-		
 
 	}
 
 }
-

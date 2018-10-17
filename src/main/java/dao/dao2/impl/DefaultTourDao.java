b425ba7ad2a6e2032e0be967ec6f8de03fa11f99
Andrii Piatakha <andrey.pyatakha@gmail.com>
2018-10-17 21:07:23 +0300

    commit

--- src/main/java/dao/dao2/impl/DefaultTourDao.java
+++ src/main/java/dao/dao2/impl/DefaultTourDao.java
@@ -1,5 +1,41 @@
 package dao.dao2.impl;
 
-public class DefaultTourDao {
+import java.util.List;
+
+import dao.dao2.TourDao;
+import dao.models.Tour;
+import dao.models.User;
+
+public class DefaultTourDao implements TourDao {
+
+	@Override
+	public Tour getTourById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean saveTour(Tour tour) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Tour> getAllTours() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Tour> getToursForUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Tour> getToursWithinDays(int range) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
